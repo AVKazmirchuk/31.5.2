6e194660004f0fa893ea21e924b3a9be26bec277
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2021-09-24 00:39:11 +0300

    Add files via upload

--- src/LGraph.cpp
+++ src/LGraph.cpp
@@ -1,19 +1,121 @@
-#include "../include/IGraph.h"
-#include "../include/lGraph.h"
+#include "IGraph.h"
+#include "LGraph.h"
+#include "MGraph.h"
 
-ListGraph::ListGraph(MatrixGraph* mg)
+//Функция инициализации графа матрицей смежности
+void ListGraph::init(const IGraph& mg)
 {
-    numberOfVertices = mg->numberOfVertices;
+	g.clear();
 
-    std::vector<int> tmp;
-    for (int j{}; j < mg->numberOfVertices; ++j)
-    {
-        for (int i{}; i < mg->numberOfVertices; ++i)
-        {
-            if (mg->g[j][i] == 1) tmp.push_back(i + 1);
-        }
+	std::vector<int> tmp;
 
-        g.push_back(tmp);
-        tmp.clear();
-    }
+	for (size_t j{}; j < mg.VerticesCount(); ++j)
+	{
+		for (size_t i{}; i < mg.VerticesCount(); ++i)
+		{
+			if (mg.getG()[j][i]) tmp.push_back(i + 1);
+		}
+
+		g.push_back(tmp);
+		tmp.clear();
+	}
+}
+
+//Функция построения списков смежности, если количество вершин заранее не известно
+void ListGraph::AddEdge(int from, int to)
+{
+	size_t numberOfVertices = VerticesCount();
+
+	//Определить текущее количество вершин
+	if (numberOfVertices < from || numberOfVertices < to)
+		numberOfVertices = (from >= to ? from : to);
+
+	std::vector<int> tmp;
+
+	//Добавить строки по текущему количеству вершин 
+	while (g.size() < numberOfVertices)
+		g.push_back(tmp);
+
+	//Если вершина связанная
+	if (to != 0) g[from - 1].push_back(to);
+}
+
+//Если вершина не имеет петли - удалить
+void ListGraph::isLoop(std::vector<int>& vertices, int vertex) const
+{
+	auto pos = std::find(g[vertex - 1].begin(), g[vertex - 1].end(), vertex);
+
+	if (pos == g[vertex - 1].end())
+	{
+		std::swap(vertices.front(), vertices.back());
+		vertices.pop_back();
+	}
+}
+
+//Все вершины, в которые можно дойти из заданной
+void ListGraph::GetNextVertices(int vertex, std::vector<int>& nextVertices) const
+{
+	nextVertices.clear();
+	
+	//Стандартный рекурсивный алгоритм (обход из начала в конец ребра)
+	std::function<void(int)> dfs = [&](int vertex)
+	{
+		nextVertices.push_back(vertex);
+
+		for (auto nextVertex : g[vertex - 1])
+			//Если вершину не посещали - переход к следующей вершине
+			if (std::find(nextVertices.begin(), nextVertices.end(), nextVertex) == nextVertices.end())
+				dfs(nextVertex);
+	};
+
+	dfs(vertex);
+
+	//Если вершина не имеет петли - удалить
+	isLoop(nextVertices, vertex);
+
+	std::sort(nextVertices.begin(), nextVertices.end());
+}
+
+//Все вершины, из которых можно дойти в заданную
+void ListGraph::GetPrevVertices(int vertex, std::vector<int>& prevVertices) const
+{
+	prevVertices.clear();
+	
+	//Стандартный рекурсивный алгоритм (обход из конца в начало ребра)
+	std::function<void(int)> dfs = [&](int vertex)
+	{
+		prevVertices.push_back(vertex);
+
+		for (size_t i{}; i < VerticesCount(); ++i)
+			for (auto nextVertex : g[i])
+				if (nextVertex == vertex)
+					//Если вершину не посещали - переход к следующей вершине
+					if (std::find(prevVertices.begin(), prevVertices.end(), i + 1) == prevVertices.end())
+						dfs(i + 1);
+	};
+
+	dfs(vertex);
+
+	//Если вершина не имеет петли - удалить
+	isLoop(prevVertices, vertex);
+
+	std::sort(prevVertices.begin(), prevVertices.end());
+}
+
+//Вывод графа
+void ListGraph::output() const
+{
+	std::cout << "\nList:\n";
+
+	for (size_t j{}; j < VerticesCount(); ++j)
+	{
+		std::cout << "g[" << j + 1 << "]: ";
+
+		for (size_t i{}; i < g[j].size(); ++i)
+		{
+			std::cout << g[j][i] << ' ';
+		}
+
+		std::cout << '\n';
+	}
 }
--- src/MGraph.cpp
+++ src/MGraph.cpp
@@ -1,21 +1,122 @@
-#include "../include/IGraph.h"
-#include "../include/lGraph.h"
-#include "../include/MGraph.h"
+#include "IGraph.h"
+#include "LGraph.h"
+#include "MGraph.h"
 
-MatrixGraph::MatrixGraph(ListGraph* lg)
+//Функция инициализации графа списками смежности
+void MatrixGraph::init(const IGraph& lg)
 {
-    numberOfVertices = lg->numberOfVertices;
+	g.clear();
 
-    std::vector<int> tmp(lg->numberOfVertices);
+	std::vector<int> tmp(lg.VerticesCount());
 
-    for (int i{}; i < lg->numberOfVertices; ++i)
-        g.push_back(tmp);
+	for (size_t i{}; i < lg.VerticesCount(); ++i)
+		g.push_back(tmp);
 
-    for (int j{}; j < lg->numberOfVertices; ++j)
-    {
-        for (int i{}; i < lg->g[j].size(); ++i)
-        {
-            g[j][lg->g[j][i]] = 1;
-        }
-    }
+	for (size_t j{}; j < lg.VerticesCount(); ++j)
+	{
+		for (size_t i{}; i < lg.getG()[j].size(); ++i)
+		{
+			g[j][lg.getG()[j][i] - 1] = 1;
+		}
+	}
+}
+
+//Функция построения матрицы смежности, если количество вершин заранее не известно
+void MatrixGraph::AddEdge(int from, int to)
+{
+	size_t numberOfVertices = VerticesCount();
+
+	//Определить текущее количество вершин
+	if (numberOfVertices < from || numberOfVertices < to)
+		numberOfVertices = (from >= to ? from : to);
+
+	std::vector<int> tmp(numberOfVertices);
+
+	//Добавить строки по текущему количеству вершин 
+	while (g.size() < numberOfVertices)
+		g.push_back(tmp);
+
+	//Заполнить предыдущие строки "0-ми" до текущего количества вершин
+	for (size_t j{}; j < g.size() - 1; ++j)
+		while (g[j].size() < numberOfVertices)
+			g[j].push_back(0);
+
+	//Если вершина несвязанная
+	if (to == 0) return;
+
+	g[from - 1][to - 1] = 1;
+}
+
+//Если вершина не имеет петли - удалить
+void MatrixGraph::isLoop(std::vector<int>& vertices, int vertex) const
+{
+	if (!g[vertex - 1][vertex - 1])
+	{
+		std::swap(vertices.front(), vertices.back());
+		vertices.pop_back();
+	}
+}
+
+//Все вершины, в которые можно дойти из заданной
+void MatrixGraph::GetNextVertices(int vertex, std::vector<int>& nextVertices) const
+{
+	nextVertices.clear();
+	
+	//Стандартный рекурсивный алгоритм (обход из начала в конец ребра)
+	std::function<void(int)> dfs = [&](int vertex)
+	{
+		nextVertices.push_back(vertex);
+
+		for (size_t i{}; i < VerticesCount(); ++i)
+			//Если вершину не посещали - переход к следующей вершине
+			if (g[vertex - 1][i] && std::find(nextVertices.begin(), nextVertices.end(), i + 1) == nextVertices.end())
+				dfs(i + 1);
+	};
+
+	dfs(vertex);
+
+	//Если вершина не имеет петли - удалить
+	isLoop(nextVertices, vertex);
+
+	std::sort(nextVertices.begin(), nextVertices.end());
+}
+
+//Все вершины, из которых можно дойти в заданную
+void MatrixGraph::GetPrevVertices(int vertex, std::vector<int>& prevVertices) const
+{
+	prevVertices.clear();
+	
+	//Стандартный рекурсивный алгоритм (обход из конца в начало ребра)
+	std::function<void(int)> dfs = [&](int vertex)
+	{
+		prevVertices.push_back(vertex);
+
+		for (size_t i{}; i < VerticesCount(); ++i)
+			//Если вершину не посещали - переход к следующей вершине
+			if (g[i][vertex - 1] && std::find(prevVertices.begin(), prevVertices.end(), i + 1) == prevVertices.end())
+				dfs(i + 1);
+	};
+
+	dfs(vertex);
+
+	//Если вершина не имеет петли - удалить
+	isLoop(prevVertices, vertex);
+
+	std::sort(prevVertices.begin(), prevVertices.end());
+}
+
+//Вывод графа
+void MatrixGraph::output() const
+{
+	std::cout << "\nMatrix:\n";
+
+	for (size_t j{}; j < VerticesCount(); ++j)
+	{
+		for (size_t i{}; i < VerticesCount(); ++i)
+		{
+			std::cout << g[j][i] << ' ';
+		}
+
+		std::cout << '\n';
+	}
 }
